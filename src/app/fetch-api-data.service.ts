9bad8b433bebd51d36f66b557557e675ad5cd4f0
Martin Karastoyanov <martin.karastoyanov@gmail.com>
2021-08-31 01:43:23 +0300

    Update fetch-api-data.service.ts

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -5,7 +5,7 @@ import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 //Declaring the api url that will provide data for the client app
-const apiUrl = 'YOUR_HOSTED_API_URL_HERE/';
+const apiUrl = 'https://myflix-21.herokuapp.com/';
 @Injectable({
   providedIn: 'root'
 })
@@ -17,10 +17,100 @@ export class UserRegistrationService {
  // Making the api call for the user registration endpoint
   public userRegistration(userDetails: any): Observable<any> {
     console.log(userDetails);
-    return this.http.post(apiUrl + 'users', userDetails).pipe(
-    catchError(this.handleError)
+    return this.http
+    .post(apiUrl + 'users', userDetails)
+    .pipe(catchError(this.handleError)
     );
   }
+
+    // User login (Endpoint: 'login', Method: POST).
+    public userLogin(userDetails: any): Observable<any> {
+      // Send the any argument to the API endpoint and return the API response.
+      return this.http
+        .post(apiUrl + 'login', userDetails)
+        .pipe(catchError(this.handleError));
+    }
+  
+    // Get user (Endpoint: 'users/:username', Method: GET).
+    public getUser(username: any): Observable<any> {
+      
+      return this.http
+        .get(apiUrl + `users/${username}`)
+        .pipe(catchError(this.handleError));
+    }
+  
+    // Edit user (Endpoint: 'users/:username', Method: PUT).
+    public editUser(userDetails: any): Observable<any> {
+      
+      return this.http
+        .put(apiUrl + `users/${userDetails.username}`, userDetails )
+        .pipe(catchError(this.handleError));
+    }
+  
+    // Delete user (Endpoint: 'users/:username', Method: DELETE).
+    public deleteUser(username: any): Observable<any> {
+    
+      return this.http
+        .delete(apiUrl + `users/${username}`)
+        .pipe(catchError(this.handleError));
+    }
+  
+    // Get Favorites list (Endpoint: 'users/:username/favorites', Method: GET).
+    public getFavorites(username: any): Observable<any> {
+      
+      return this.http
+        .get(apiUrl + `users/${username}/favorites`)
+        .pipe(catchError(this.handleError));
+    }
+  
+    // Add movie to Favorites list (Endpoint: 'users/:username/movies/:movie_id', Method: POST).
+    public addMovieFavorites(username: any, movieId: any): Observable<any> {
+      
+      return this.http
+        .post(apiUrl + `users/${username}/movies/${movieId}`)
+        .pipe(catchError(this.handleError));
+    }
+  
+    // Remove movie from Favorites list (Endpoint: 'users/:username/movies/:movie_id', Method: DELETE).
+    public removeMovieFavorites(username: any, movieId: any): Observable<any> {
+      
+      return this.http
+        .delete(apiUrl + `users/${username}/movies/${movieId}`)
+        .pipe(catchError(this.handleError));
+    }
+
+  
+    // Get all movies endpoint (Endpoint: 'movies', Method: GET).
+    public getAllMovies(): Observable<any> {
+      
+      return this.http
+        .get(apiUrl + 'movies')
+        .pipe(catchError(this.handleError));
+    }
+  
+    // Get one movie endpoint (Endpoint: 'movies/:title', Method: GET).
+    public getMovie(movieTitle: any): Observable<any> {
+      
+      return this.http
+        .get(apiUrl + `movies/${movieTitle}`)
+        .pipe(catchError(this.handleError));
+      }
+  
+    // Get one genre endpoint (Endpoint: 'genres/:name', Method: GET).
+    public getGenre(genreName: any): Observable<any> {
+      
+      return this.http
+        .get(apiUrl + `genres/${genreName}`)
+        .pipe(catchError(this.handleError));
+    }
+  
+    // Get one director endpoint (Endpoint: 'directors/:name', Method: GET).
+    public getDirector(directorName: any): Observable<any> {
+      
+      return this.http
+        .get(apiUrl + `directors/${directorName}`)
+        .pipe(catchError(this.handleError));
+    }
 
 private handleError(error: HttpErrorResponse): any {
     if (error.error instanceof ErrorEvent) {
